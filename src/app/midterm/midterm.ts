707cdf5f8bc8bff69b12843c5ea35318ecff1f96
locwa <94048557+locwa@users.noreply.github.com.>
2025-12-10 22:41:55 +0800

    finished midterm page

--- src/app/midterm/midterm.ts
+++ src/app/midterm/midterm.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-midterm',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './midterm.html',
   styleUrl: './midterm.css',
 })
 export class Midterm {
+  imgLinks = [
+    "/mglab1.png",
+    "/mglab2.png",
+    "/mglab3.png",
+    "/mglab4.png",
+    "/mgexam.png"
+  ]
 
+  isHovered = false
+  imgSrc = ""
+
+  changePhoto(index: number){
+    this.imgSrc = this.imgLinks[index]
+    this.isHovered = true
+  }
+
+  removePhoto(){
+    this.isHovered = false;
+  }
 }
