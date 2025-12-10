c3f5b4787ec8253d20996a098ae08c473a4a7e4f
locwa <94048557+locwa@users.noreply.github.com.>
2025-12-10 22:52:55 +0800

    finished finals page

--- src/app/finals/finals.ts
+++ src/app/finals/finals.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-finals',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './finals.html',
   styleUrl: './finals.css',
 })
 export class Finals {
+  imgLinks = [
+    "/finalscoursework.png",
+    "/finalslab1.png",
+    "/finalslab2.png",
+    "/finalslab3.png",
+    "/finalproject.png"
+  ]
 
+  isHovered = false
+  imgSrc = ""
+
+  changePhoto(index: number){
+    this.imgSrc = this.imgLinks[index]
+    this.isHovered = true
+  }
+
+  removePhoto(){
+    this.isHovered = false;
+  }
 }
